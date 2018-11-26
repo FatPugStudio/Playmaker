9403c6b6ba0789e8ac9e89e95a52ff3cf2902f64
Fat Pug Studio <Oshigawa@users.noreply.github.com>
2018-11-26 14:17:52 +0100

    Create EnableCanvas.cs

---
+++ EnableCanvas.cs
@@ -0,0 +1,42 @@
+// License: Attribution 4.0 International (CC BY 4.0)
+/*--- __ECO__ __PLAYMAKER__ __ACTION__ ---*/
+
+using UnityEngine;
+
+namespace HutongGames.PlayMaker.Actions
+{
+    [ActionCategory(ActionCategory.Physics)]
+    [Tooltip("Enables or disables Canvas")]
+    [HelpUrl("")]
+    public class EnableCanvas : ComponentAction<Canvas>
+    {
+        [RequiredField]
+        [CheckForComponent(typeof(Canvas))]
+        [Tooltip("Canvas to enable.")]
+        public FsmOwnerDefault gameObject;
+	
+		[Tooltip("Enabled")]
+        public FsmBool enabled;
+       
+        public override void Reset()
+        {
+            gameObject = null;
+			enabled = false;
+        }
+
+        public override void OnEnter()
+        {
+            DoChange();
+
+        }
+
+        void DoChange()
+        
+		{
+            var go = Fsm.GetOwnerDefaultTarget(gameObject);
+            Canvas canvas = go.GetComponent<Canvas>();
+			canvas.enabled = enabled.Value;
+                   
+        }
+    }
+}
